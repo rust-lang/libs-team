2011561e352f23acd93ab9e7afbdb04e8760d57c
Mara Bos <m-ou.se@m-ou.se>
2021-03-10 23:08:10 +0100

    Automatically pipe output through rustfmt and bat.

--- tools/unstable-api/src/main.rs
+++ tools/unstable-api/src/main.rs
@@ -27,6 +27,9 @@ fn main() -> Result<(), Error> {
         None => find_repo_root()?,
     };
 
+    #[cfg(unix)]
+    setup_output_formatting();
+
     let libs = vec![
         repo_root.clone().join("library/core"),
         repo_root.clone().join("library/alloc"),
@@ -51,4 +54,35 @@ fn find_repo_root() -> Result<PathBuf, Error> {
     let mut path = PathBuf::from(String::from_utf8(path)?);
     path.pop();
     Ok(path)
+}
+
+#[cfg(unix)]
+fn setup_output_formatting() {
+    use nix::unistd::{isatty, dup2};
+    use std::os::unix::io::AsRawFd;
+    use std::process::{Command, Stdio};
+
+    if isatty(1) == Ok(true) {
+        // Pipe the output through `bat` for nice formatting and paging, if available.
+        if let Ok(mut bat) = Command::new("bat")
+            .arg("--language=rust")
+            .arg("--plain") // Disable line numbers for easy copy-pasting.
+            .stdin(Stdio::piped())
+            .stdout(Stdio::inherit())
+            .spawn()
+        {
+            // Replace our stdout by the pipe into `bat`.
+            dup2(bat.stdin.take().unwrap().as_raw_fd(), 1).unwrap();
+        }
+    }
+
+    // Pipe the output through `rustfmt`, if available.
+    if let Ok(mut rustfmt) = Command::new("rustfmt")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::inherit()) // This pipes into `bat` if it was executed above.
+        .spawn()
+    {
+        // Replace our stdout by the pipe into `rustfmt`.
+        dup2(rustfmt.stdin.take().unwrap().as_raw_fd(), 1).unwrap();
+    }
 }
