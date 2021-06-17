af8f2f8dd4001dbe332254b928a8d9480ea662ae
Jane Lusby <jlusby@yaah.dev>
2021-06-17 16:42:09 -0700

    Add support for generating a non api libs agenda

---
+++ tools/agenda-generator/src/cli.rs
@@ -0,0 +1,22 @@
+use structopt::StructOpt;
+use structopt::clap::arg_enum;
+
+#[derive(Debug, StructOpt)]
+pub struct Args {
+    #[structopt(long, short, default_value = "LibsAPI", possible_values = &AgendaKind::variants())]
+    pub agenda: AgendaKind,
+}
+
+impl Args {
+    pub fn from_args() -> Args {
+        StructOpt::from_args()
+    }
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum AgendaKind {
+        Libs,
+        LibsAPI,
+    }
+}
