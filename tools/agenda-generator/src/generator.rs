8ba18a05298499557d89fb735e21d77a65e2b8cd
ltdk <usr@ltdk.xyz>
2026-09-13 15:36:21 -0400

    rust.tf/stdarchNNNN

--- tools/agenda-generator/src/generator.rs
+++ tools/agenda-generator/src/generator.rs
@@ -63,6 +63,10 @@ fn shorten(url: &str) -> String {
         format!("rust.tf/libs{num}")
     } else if let Some(num) = url.strip_prefix("https://github.com/rust-lang/libs-team/pull/") {
         format!("rust.tf/libs{num}")
+    } else if let Some(num) = url.strip_prefix("https://github.com/rust-lang/stdarch/issues/") {
+        format!("rust.tf/stdarch{num}")
+    } else if let Some(num) = url.strip_prefix("https://github.com/rust-lang/stdarch/pull/") {
+        format!("rust.tf/stdarch{num}")
     } else if let Some(url) = url.strip_prefix("https://") {
         url.to_string()
     } else {
