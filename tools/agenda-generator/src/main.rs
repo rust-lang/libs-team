f9ff77b74a01444a68ee58783206f47e8e71da88
ltdk <usr@ltdk.xyz>
2026-09-19 11:27:48 -0400

    Clean up agenda

--- tools/agenda-generator/src/main.rs
+++ tools/agenda-generator/src/main.rs
@@ -1,18 +1,39 @@
-use cli::AgendaKind;
 use color_eyre::eyre;
 use generator::Generator;
 
-mod cli;
 mod generator;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Parser)]
+pub struct Args {
+    #[clap(long, short, default_value = "Libs")]
+    pub agenda: AgendaKind,
+}
+
+impl Args {
+    pub fn from_args() -> Args {
+        <Args as Parser>::parse()
+    }
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+#[clap(rename_all = "verbatim")]
+pub enum AgendaKind {
+    Libs,
+    PgEh,
+}
 
 fn main() -> eyre::Result<()> {
     color_eyre::install()?;
-    let args = cli::Args::from_args();
+    let args = Args::from_args();
     let generator = Generator::default();
     let agenda = match args.agenda {
         AgendaKind::Libs => generator.libs_agenda()?,
-        AgendaKind::PGEH => generator.error_handling_pg_agenda()?,
+        AgendaKind::PgEh => generator.error_handling_pg_agenda()?,
     };
-    println!("{}", agenda);
+
+    // note: newline included
+    print!("{}", agenda);
     Ok(())
 }
